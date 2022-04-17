dc94e358258b3cd8fb3570b832d0664603998a7b
Andrew <healeycodes@gmail.com>
2022-04-17 12:26:46 +0100

    Allow cat to receive multiple files

--- kat/main.go
+++ kat/main.go
@@ -9,25 +9,23 @@ import (
 )
 
 func main() {
-	var r *bufio.Reader
 	if len(os.Args) > 1 {
-		f, err := os.Open(os.Args[1])
-		if err != nil {
-			log.Fatalf("File error: %s, ", err)
+		for _, fp := range os.Args[1:] {
+			f, err := os.Open(fp)
+			if err != nil {
+				log.Fatalf("File error: %s, ", err)
+			}
+			utils.StreamFromReader(bufio.NewReader(f))
 		}
-
-		r = bufio.NewReader(f)
 	} else {
 		info, err := os.Stdin.Stat()
 		if err != nil {
 			log.Fatalf("Pipe error: %s", err)
 		}
 		if (info.Mode()&os.ModeCharDevice) == 0 || info.Size() <= 0 {
-			r = bufio.NewReader(os.Stdin)
+			utils.StreamFromReader(bufio.NewReader(os.Stdin))
 		} else {
 			log.Fatal("No filepath argument or pipe input")
 		}
 	}
-
-	utils.StreamFromReader(r)
 }
